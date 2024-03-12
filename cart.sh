b668da5a58a650ea1b4af47b56d90af49590f352
lavanya <lavanyaramesh319@gmail.com>
2024-03-12 20:36:22 +0530

    updated the code

--- cart.sh
+++ cart.sh
@@ -1,14 +1,32 @@
+echo -e "\e[36m>>>>>>>>>> Configuring Nodejs repos <<<<<<<<<<\e[0m"
 dnf module disable nodejs -y
 dnf module enable nodejs:18 -y
+
+echo -e "\e[36m>>>>>>>>>> Install Nodejs <<<<<<<<<<\e[0m"
 dnf install nodejs -y
+
+echo -e "\e[36m>>>>>>>>>> add application user <<<<<<<<<<\e[0m"
 useradd roboshop
+
+echo -e "\e[36m>>>>>>>>>> Create application directory <<<<<<<<<<\e[0m"
+rm -rf /app
 mkdir /app
+
+echo -e "\e[36m>>>>>>>>>> download cart content <<<<<<<<<<\e[0m"
 curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
 cd /app
+
+echo -e "\e[36m>>>>>>>>>> unzip user content <<<<<<<<<<\e[0m"
 unzip /tmp/cart.zip
 cd /app
+
+echo -e "\e[36m>>>>>>>>>> Nodejs dependencies <<<<<<<<<<\e[0m"
 npm install
-cp cart.service /etc/systemd/system/cart.service
+
+echo -e "\e[36m>>>>>>>>>> copy user system file <<<<<<<<<<\e[0m"
+cp /home/centos/Roboshop/cart.service /etc/systemd/system/cart.service
+
+echo -e "\e[36m>>>>>>>>>> start user service <<<<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable cart
-systemctl start cart
+systemctl restart cart
--- user.sh
+++ user.sh
@@ -1,18 +1,41 @@
+echo -e "\e[36m>>>>>>>>>> Configuring Nodejs repos <<<<<<<<<<\e[0m"
 dnf module disable nodejs -y
 dnf module enable nodejs:18 -y
+
+echo -e "\e[36m>>>>>>>>>> Install Nodejs <<<<<<<<<<\e[0m"
 dnf install nodejs -y
+
+echo -e "\e[36m>>>>>>>>>> add application user <<<<<<<<<<\e[0m"
 useradd roboshop
+
+echo -e "\e[36m>>>>>>>>>> Create application directory <<<<<<<<<<\e[0m"
+rm -rf /app
 mkdir /app
+
+echo -e "\e[36m>>>>>>>>>> download app content <<<<<<<<<<\e[0m"
 curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
 cd /app
+
+echo -e "\e[36m>>>>>>>>>> unzip app content <<<<<<<<<<\e[0m"
 unzip /tmp/user.zip
 cd /app
+
+echo -e "\e[36m>>>>>>>>>> Nodejs dependencies <<<<<<<<<<\e[0m"
 npm install
-cp user.service /etc/systemd/system/user.service
+
+echo -e "\e[36m>>>>>>>>>> copy user system file <<<<<<<<<<\e[0m"
+cp /home/centos/Roboshop/user.service /etc/systemd/system/user.service
+
+echo -e "\e[36m>>>>>>>>>> start user service <<<<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable user
-systemctl start user
+systemctl restart user
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+echo -e "\e[36m>>>>>>>>>> Copy mongodb repo <<<<<<<<<<\e[0m"
+cp /home/centos/Roboshop/mongo.repo /etc/yum.repos.d/mongo.repo
+
+echo -e "\e[36m>>>>>>>>>> Install mongodb <<<<<<<<<<\e[0m"
 dnf install mongodb-org-shell -y
-mongo --host mongodb.devopz1.online </app/schema/user.js
+
+echo -e "\e[36m>>>>>>>>>> Load schema <<<<<<<<<<\e[0m"
+mongo --host mongodb-dev.devopz1.online </app/schema/user.js
