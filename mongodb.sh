5c0f04bd4ba191ea867f42cbad052d56a74907b1
lavanya <lavanyaramesh319@gmail.com>
2024-03-12 12:25:42 +0530

    mongodb script changes

--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,8 @@
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-org -y
+
+sed -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
+
 systemctl enable mongod
 systemctl start mongod
 
-# edit the file and replace 127.0.0.1 to 0.0.0.0
