ebaa36c33e4ccd3b99332685dcbd7b7a87e48476
lavanya <lavanyaramesh319@gmail.com>
2024-03-12 20:50:39 +0530

    mysql service

--- shipping.sh
+++ shipping.sh
@@ -1,15 +1,34 @@
+echo -e "\e[36m>>>>>>>>>> Install Maven<<<<<<<<<\e[0m"
 dnf install maven -y
+
+echo -e "\e[36m>>>>>>>>>>create app user<<<<<<<<<\e[0m"
 useradd roboshop
+
+echo -e "\e[36m>>>>>>>>>>create app directory<<<<<<<<<\e[0m"
+rm -rf /app
 mkdir /app
+
+echo -e "\e[36m>>>>>>>>>>Download app content<<<<<<<<<\e[0m"
 curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
+
+echo -e "\e[36m>>>>>>>>>>extract app<<<<<<<<<\e[0m"
 cd /app
+
+echo -e "\e[36m>>>>>>>>>>unzip shipping <<<<<<<<<\e[0m"
 unzip /tmp/shipping.zip
-cd /app
+
+echo -e "\e[36m>>>>>>>>>>download maven dependencies<<<<<<<<<\e[0m"
 mvn clean package
 mv target/shipping-1.0.jar shipping.jar
+
+echo -e "\e[36m>>>>>>>>>>install mysql<<<<<<<<<\e[0m"
+dnf install mysql -y
+
+echo -e "\e[36m>>>>>>>>>>Load schema<<<<<<<<<\e[0m"
+mysql -h mysql-dev.devopz1.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
+systemctl restart shipping
+
+echo -e "\e[36m>>>>>>>>>>start shipping service<<<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable shipping
-systemctl start shipping
-dnf install mysql -y
-mysql -h mysql-dev.devopz1.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
 systemctl restart shipping
