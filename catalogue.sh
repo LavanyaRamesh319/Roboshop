42ec561105da26af39c8ecbe20aed560ea75806f
lavanya <lavanyaramesh319@gmail.com>
2024-03-12 14:38:01 +0530

    adding print statements

--- catalogue.sh
+++ catalogue.sh
@@ -9,6 +9,7 @@ echo -e "\e[36m>>>>>>>>>> add application user <<<<<<<<<<\e[0m"
 useradd roboshop
 
 echo -e "\e[36m>>>>>>>>>> Create application directory <<<<<<<<<<\e[0m"
+rm -rf /app
 mkdir /app
 
 echo -e "\e[36m>>>>>>>>>> download app content <<<<<<<<<<\e[0m"
