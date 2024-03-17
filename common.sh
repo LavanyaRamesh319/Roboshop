6f566812a52a310a8cbe16176422556cfca3f063
lavanya <lavanyaramesh319@gmail.com>
2024-03-18 03:23:49 +0530

    exit status added

--- common.sh
+++ common.sh
@@ -5,51 +5,77 @@ script_path=$(dirname "$script")
 func_print_head() {
   echo -e "\e[36m>>>>>>>>>> $1 <<<<<<<<<<\e[0m"
 }
-
+func_status_check(){
+  if [ $1 -eq 0 ]; then
+        echo -e "\e[32mSUCCESS\e[0m"
+        else
+        echo -e "\e32mFAILURE\e[0m"
+        exit 1
+  fi
+}
 func_schema_setup(){
   if [ "$schema_setup" == "mongo" ]; then
   func_print_head  "Copy mongodb"
   cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
+    func_status_check $?
+
 
   func_print_head  "Install mongodb"
   dnf install mongodb-org-shell -y
+    func_status_check $?
+
 
   func_print_head  "Load schema"
   mongo --host mongodb-dev.devopz1.online </app/schema/${component}.js
+    func_status_check $?
+
   fi
   if ["${schema_setup}"=="mysql" ]; then
   func_print_head "install mysql"
   dnf install mysql -y
+    func_status_check $?
+
 
   func_print_head "Load schema"
   mysql -h mysql-dev.devopz1.online -uroot -p${mysql_root_password} < /app/schema/${component}.sql
-  fi
+    func_status_check $?
+fi
 }
+
 func_app_prereq(){
 
   func_print_head "create application user"
   useradd ${app_user}
+    func_status_check $?
+
 
   func_print_head "create application directory"
   rm -rf /app
   mkdir /app
+  func_status_check $?
 
   func_print_head "Download application content"
   curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
+    func_status_check $?
 
   func_print_head "extract application content"
   cd /app
   unzip /tmp/${component}.zip
+    func_status_check $?
+
 }
 
 func_systemd_setup(){
         func_print_head "setup systemd service"
          cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
+          func_status_check $?
 
          func_print_head "start ${component} service"
          systemctl daemon-reload
          systemctl enable ${component}
          systemctl restart ${component}
+         func_status_check $?
+
 }
 
 func_nodejs(){
@@ -60,11 +86,15 @@ func_print_head "Configuring Nodejs repos"
 
 func_print_head "Install Nodejs"
   dnf install nodejs -y
+  func_status_check $?
+
 
 func_app_prereq
 
 func_print_head "Nodejs dependencies"
   npm install
+  func_status_check $?
+
 
   func_schema_setup
   func_systemd_setup
@@ -74,21 +104,14 @@ func_print_head "Nodejs dependencies"
 func_java(){
   func_print_head "Install Maven"
   dnf install maven -y
-  if [ $? -eq 0 ]; then
-    echo -e "\e[32mSUCCESS\e[0m"
-    else
-    echo -e "\e32mFAILURE\e[0m"
-  fi
+  func_status_check $?
+
 
   func_app_prereq
 
   func_print_head "download maven dependencies"
   mvn clean package
-  if [ $? -eq 0 ]; then
-      echo -e "\e[32mSUCCESS\e[0m"
-      else
-      echo -e "\e32mFAILURE\e[0m"
-  fi
+  func_status_check $?
   mv target/${component}-1.0.jar ${component}.jar
 
   func_schema_setup
