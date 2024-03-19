0b9f58ce04552cb52c16d4a43ccf928cb42ec20a
lavanya <lavanyaramesh319@gmail.com>
2024-03-20 05:04:46 +0530

    mysql changes

--- mysql.sh
+++ mysql.sh
@@ -8,18 +8,23 @@ if [ -z $mysql_root_password ]; then
   exit
 fi
 
-echo -e "\e[36m>>>>>>>>>>disable mysql default version<<<<<<<<<\e[0m"
-dnf module disable mysql -y
+func_print_head "disable mysql default version"
+dnf module disable mysql -y &>>$log_file
+func_status_check $?
 
-echo -e "\e[36m>>>>>>>>>>copy mysql repo file<<<<<<<<<\e[0m"
-cp ${script_path}//mysql.repo /etc/yum.repos.d/mysql.repo
+func_print_head "copy mysql repo file"
+cp ${script_path}//mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
+func_status_check $?
 
-echo -e "\e[36m>>>>>>>>>>install mysql default version<<<<<<<<<\e[0m"
-dnf install mysql-community-server -y
+func_print_head "install mysql default version"
+dnf install mysql-community-server -y &>>$log_file
+func_status_check $?
 
-echo -e "\e[36m>>>>>>>>>>start mysql default version<<<<<<<<<\e[0m"
-systemctl enable mysqld
-systemctl restart mysqld
+func_print_head "start mysql default version"
+systemctl enable mysqld &>>$log_file
+systemctl restart mysqld &>>$log_file
+func_status_check $?
 
-echo -e "\e[36m>>>>>>>>>>reset mysql password <<<<<<<<<\e[0m"
-mysql_secure_installation --set-root-pass $mysql_root_password
+func_print_head "reset mysql password"
+mysql_secure_installation --set-root-pass $mysql_root_password &>>$log_file
+func_status_check $?
