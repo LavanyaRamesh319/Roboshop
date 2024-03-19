f783d11cb2f03c3f558c89d37b4b74e93db62c24
lavanya <lavanyaramesh319@gmail.com>
2024-03-19 12:42:03 +0530

    frontend changes

--- frontend.sh
+++ frontend.sh
@@ -4,26 +4,26 @@ source ${script_path}/common.sh
 
 func_print_head "install nginx"
 dnf install nginx -y &>>log_file
-func_stat_check $?
+func_status_check $?
 
 func_print_head "copy roboshop content"
 cp roboshop.conf /etc/nginx/default.d/roboshop.conf &>>log_file
-func_stat_check $?
+func_status_check $?
 
 func_print_head "remove old app content"
 rm -rf /usr/share/nginx/html/* &>>log_file
-func_stat_check $?
+func_status_check $?
 
 func_print_head "download app content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>log_file
-func_stat_check $?
+func_status_check $?
 
 func_print_head "unzip frontend content"
 cd /usr/share/nginx/html &>>log_file
 unzip /tmp/frontend.zip &>>log_file
-func_stat_check $?
+func_status_check $?
 
 func_print_head "start service"
 systemctl restart nginx &>>log_file
 systemctl enable nginx &>>log_file
-func_stat_check $?
+func_status_check $?
