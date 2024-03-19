dbe6f419c5fa93f69f42af683fd213f25fa4c6e1
lavanya <lavanyaramesh319@gmail.com>
2024-03-19 12:37:28 +0530

    payment changes

--- frontend.sh
+++ frontend.sh
@@ -2,22 +2,28 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
-echo -e "\e[36m>>>>>>>>>>install nginx<<<<<<<<<\e[0m"
-dnf install nginx -y
+func_print_head "install nginx"
+dnf install nginx -y &>>log_file
+func_stat_check $?
 
-echo -e "\e[36m>>>>>>>>>>copy roboshop content<<<<<<<<<\e[0m"
-cp roboshop.conf /etc/nginx/default.d/roboshop.conf
+func_print_head "copy roboshop content"
+cp roboshop.conf /etc/nginx/default.d/roboshop.conf &>>log_file
+func_stat_check $?
 
-echo -e "\e[36m>>>>>>>>>>remove nginx <<<<<<<<<\e[0m"
-rm -rf /usr/share/nginx/html/*
+func_print_head "remove old app content"
+rm -rf /usr/share/nginx/html/* &>>log_file
+func_stat_check $?
 
-echo -e "\e[36m>>>>>>>>>>frontend content<<<<<<<<<\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-cd /usr/share/nginx/html
+func_print_head "download app content"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>log_file
+func_stat_check $?
 
-echo -e "\e[36m>>>>>>>>>>unzip frontend content<<<<<<<<<\e[0m"
-unzip /tmp/frontend.zip
+func_print_head "unzip frontend content"
+cd /usr/share/nginx/html &>>log_file
+unzip /tmp/frontend.zip &>>log_file
+func_stat_check $?
 
-echo -e "\e[36m>>>>>>>>>>start service<<<<<<<<<\e[0m"
-systemctl restart nginx
-systemctl enable nginx
+func_print_head "start service"
+systemctl restart nginx &>>log_file
+systemctl enable nginx &>>log_file
+func_stat_check $?
